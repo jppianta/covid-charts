0719abd25c46801836fc5b978cdfdf16c82e89a4
jppianta <joao.pianta@outlook.com>
2020-04-01 01:08:17 -0300

    new build

---
+++ docs/precache-manifest.f098c8bef94aa77c4c4fe13aa33ebbc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850b64d444f52ac8eec9d9de32b88b06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7dc6e6cce52ce051b11a",
+    "url": "./static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "6ab786953d630d9fd935",
+    "url": "./static/css/main.594ea12e.chunk.css"
+  },
+  {
+    "revision": "7dc6e6cce52ce051b11a",
+    "url": "./static/js/2.231aa573.chunk.js"
+  },
+  {
+    "revision": "c7f03fa1b8bf778d38ecc0e40b7777e7",
+    "url": "./static/js/2.231aa573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab786953d630d9fd935",
+    "url": "./static/js/main.4f2e2077.chunk.js"
+  },
+  {
+    "revision": "8aff2edd53aa41c8856e",
+    "url": "./static/js/runtime-main.ddb1ce51.js"
+  },
+  {
+    "revision": "807e241cba739c7b5f8458a9f214db64",
+    "url": "./static/media/Covid-19-Charts.807e241c.png"
+  }
+]);
